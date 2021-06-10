f53727eb9419b19a695a5d8ddce54b03a1140dc4
Xavier <xavier.zentilin@gmail.com>
2021-06-10 12:23:07 +0200

    Tutorial 2

--- AleXavSharedTesting/Assets/DudeBehaviour.cs
+++ AleXavSharedTesting/Assets/DudeBehaviour.cs
@@ -11,6 +11,8 @@ public class DudeBehaviour : MonoBehaviour
     private SpriteRenderer spriteRenderer;
     private TextMeshProUGUI textMesh;
     private float nextDialogTimer;
+    private int dialogFlowState;
+    private bool mistakeDone;
 
     public Sprite dudeHappy;
     public Sprite dudeFine;
@@ -22,22 +24,24 @@ public class DudeBehaviour : MonoBehaviour
         sceneName = SceneManager.GetActiveScene().name;
         spriteRenderer = this.GetComponent<SpriteRenderer>();
         textMesh = this.transform.GetChild(0).transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+        nextDialogTimer = 0;
+        dialogFlowState = 0;
+        mistakeDone = false;
 
         if (sceneName.Equals("Level 1"))
         {
-            nextDialogTimer = 0;
-            textMesh.text = DialogsUtility.dialogs[DialogInstance.Welcome].Text;
+            textMesh.text = DialogsUtility.dialogs[DialogInstance.Welcome];
         }
         else if (sceneName.Equals("Level 2"))
         {
-
+            textMesh.text = DialogsUtility.dialogs[DialogInstance.WelcomeAgain];
         }
         else
         {
 
         }
 
-        spriteRenderer.sprite = dudeFine;
+        spriteRenderer.sprite = dudeHappy;
     }
 
     // Update is called once per frame
@@ -65,88 +69,230 @@ public class DudeBehaviour : MonoBehaviour
         var lowerPin = MapUtility.LowerPins.First();
 
         //se stacchi il cavo, fai arrabbiare Dude
-        if (!upperPin.IsConnected && !lowerPin.IsConnected && 
-            (DialogsUtility.dialogs[DialogInstance.GoToLowerPin].AlreadyShowed || DialogsUtility.dialogs[DialogInstance.GoToLowerPinSavagePartOne].AlreadyShowed))
+        if (!upperPin.IsConnected && !lowerPin.IsConnected &&
+            (dialogFlowState == 2 || dialogFlowState == 3))
         {
-            if (!DialogsUtility.dialogs[DialogInstance.Pissed].AlreadyShowed)
-                nextDialogTimer = 0;
+            nextDialogTimer = 0;
+            dialogFlowState = 4;
+            mistakeDone = true;
 
             spriteRenderer.sprite = dudePissed;
-            textMesh.text = DialogsUtility.dialogs[DialogInstance.Pissed].Text;
-            DialogsUtility.dialogs[DialogInstance.Pissed].AlreadyShowed = true;
+            textMesh.text = DialogsUtility.dialogs[DialogInstance.Pissed];
         }
 
         //Dude ti da la possibilità di rimediare (l'hai già fatto arrabbiare almeno una volta)
-        if (nextDialogTimer >= 3 && !upperPin.IsConnected && !lowerPin.IsConnected &&
-            DialogsUtility.dialogs[DialogInstance.Pissed].AlreadyShowed)
+        if (nextDialogTimer >= 6 && !upperPin.IsConnected && !lowerPin.IsConnected &&
+            dialogFlowState == 4)
         {
             spriteRenderer.sprite = dudePissed;
-            textMesh.text = DialogsUtility.dialogs[DialogInstance.RedeemYourself].Text;
-
-            DialogsUtility.dialogs[DialogInstance.RedeemYourself].AlreadyShowed = true;
+            textMesh.text = DialogsUtility.dialogs[DialogInstance.RedeemYourself];
+            dialogFlowState = 5;
         }
 
         //primo dialogo
-        if (nextDialogTimer > 6 && !upperPin.IsConnected &&
-            !DialogsUtility.dialogs[DialogInstance.GoToLowerPinSavagePartOne].AlreadyShowed &&
-            !DialogsUtility.dialogs[DialogInstance.GoToLowerPin].AlreadyShowed)
+        if (nextDialogTimer > 10 && !upperPin.IsConnected &&
+            dialogFlowState == 0)
         {
-            spriteRenderer.sprite = dudeFine;
-            textMesh.text = DialogsUtility.dialogs[DialogInstance.GoToUpperPin].Text;
-
             nextDialogTimer = 0;
-            DialogsUtility.dialogs[DialogInstance.GoToUpperPin].AlreadyShowed = true;
+            dialogFlowState = 1;
+            spriteRenderer.sprite = dudeFine;
+            textMesh.text = DialogsUtility.dialogs[DialogInstance.GoToUpperPin];
         }
 
         //se primo dialogo skippato... parte 1
-        if (nextDialogTimer <= 6 && upperPin.IsConnected && !lowerPin.IsConnected &&
-            !DialogsUtility.dialogs[DialogInstance.GoToUpperPin].AlreadyShowed)
+        if (nextDialogTimer <= 10 && upperPin.IsConnected && !lowerPin.IsConnected &&
+            dialogFlowState == 0)
         {
-            if(!DialogsUtility.dialogs[DialogInstance.GoToLowerPinSavagePartOne].AlreadyShowed)
-                nextDialogTimer = 0;
-
+            nextDialogTimer = 0;
+            dialogFlowState = 3;
             //da cambiare con dude wow
             spriteRenderer.sprite = dudeHappy;
-            textMesh.text = DialogsUtility.dialogs[DialogInstance.GoToLowerPinSavagePartOne].Text;
-
-            DialogsUtility.dialogs[DialogInstance.GoToLowerPinSavagePartOne].AlreadyShowed = true; 
+            textMesh.text = DialogsUtility.dialogs[DialogInstance.GoToLowerPinSavagePartOne];
         }
 
         //se non hai già fatto arrabbiare Dude una volta
-        if(!DialogsUtility.dialogs[DialogInstance.Pissed].AlreadyShowed)
-        //se primo dialogo skippato... parte 2
-            if (nextDialogTimer >= 3 && upperPin.IsConnected && !lowerPin.IsConnected &&
-                DialogsUtility.dialogs[DialogInstance.GoToLowerPinSavagePartOne].AlreadyShowed &&
-                !DialogsUtility.dialogs[DialogInstance.GoToLowerPin].AlreadyShowed)
+        if (!mistakeDone)
+            //se primo dialogo skippato... parte 2
+            if (nextDialogTimer >= 6 && upperPin.IsConnected && !lowerPin.IsConnected &&
+                dialogFlowState == 3)
             {
+                dialogFlowState = 2;
                 spriteRenderer.sprite = dudeFine;
-                textMesh.text = DialogsUtility.dialogs[DialogInstance.GoToLowerPinSavagePartTwo].Text;
-
-                DialogsUtility.dialogs[DialogInstance.GoToLowerPinSavagePartTwo].AlreadyShowed = true;
+                textMesh.text = DialogsUtility.dialogs[DialogInstance.GoToLowerPinSavagePartTwo];
             }
 
         //altrimenti, prosegui normale
-        if (upperPin.IsConnected && !lowerPin.IsConnected && 
-            ((!DialogsUtility.dialogs[DialogInstance.GoToLowerPinSavagePartOne].AlreadyShowed && DialogsUtility.dialogs[DialogInstance.GoToUpperPin].AlreadyShowed) ||
-             DialogsUtility.dialogs[DialogInstance.RedeemYourself].AlreadyShowed))
+        if (upperPin.IsConnected && !lowerPin.IsConnected &&
+            (dialogFlowState == 1 || dialogFlowState == 5 || dialogFlowState == 4))
         {
+            dialogFlowState = 2;
             spriteRenderer.sprite = dudeFine;
-            textMesh.text = DialogsUtility.dialogs[DialogInstance.GoToLowerPin].Text;
-
-            DialogsUtility.dialogs[DialogInstance.GoToLowerPin].AlreadyShowed = true;
+            textMesh.text = DialogsUtility.dialogs[DialogInstance.GoToLowerPin];
         }
 
         //goal
-        if (upperPin.IsConnected && lowerPin.IsConnected)
+        if (upperPin.IsConnected && lowerPin.IsConnected &&
+            dialogFlowState == 2)
         {
             spriteRenderer.sprite = dudeHappy;
-            textMesh.text = DialogsUtility.dialogs[DialogInstance.GoalReached].Text;
-        }   
+            textMesh.text = DialogsUtility.dialogs[DialogInstance.GoalReached];
+        }
     }
 
     public void SecondTutorialBehavoiur()
     {
+        nextDialogTimer += Time.deltaTime;
 
+        var blueUpperPin = MapUtility.UpperPins.ElementAt(0);
+        var pinkUpperPin = MapUtility.UpperPins.ElementAt(1);
+        var lowerPin = MapUtility.LowerPins.First();
+
+        //primo dialogo
+        if (nextDialogTimer >= 10 && !blueUpperPin.IsConnected && !pinkUpperPin.IsConnected &&
+            dialogFlowState == 0)
+        {
+            dialogFlowState = 1;
+            nextDialogTimer = 0;
+            spriteRenderer.sprite = dudeFine;
+            textMesh.text = DialogsUtility.dialogs[DialogInstance.CanDetach];
+        }
+
+        //speciale: salta spiegazione
+        if ((blueUpperPin.IsConnected || pinkUpperPin.IsConnected) &&
+            (dialogFlowState == 0 || dialogFlowState == 1))
+        {
+            dialogFlowState = 11;
+            //da sostituire con wow
+            spriteRenderer.sprite = dudeHappy;
+            textMesh.text = DialogsUtility.dialogs[DialogInstance.NoTimeToLose];
+        }
+
+        //secondo dialogo
+        if (nextDialogTimer > 10 && !blueUpperPin.IsConnected && !pinkUpperPin.IsConnected &&
+            dialogFlowState == 1)
+        {
+            dialogFlowState = 2;
+            nextDialogTimer = 0;
+            spriteRenderer.sprite = dudeFine;
+            textMesh.text = DialogsUtility.dialogs[DialogInstance.CreateBlue];
+        }
+
+        //speciale: hai scelto di collegare il rosa anche se Dude ti ha suggerito blu
+        if (!blueUpperPin.IsConnected && pinkUpperPin.IsConnected &&
+            (dialogFlowState == 2 || dialogFlowState == 8))
+        {
+            dialogFlowState = 7;
+            mistakeDone = true;
+            spriteRenderer.sprite = dudePissed;
+            textMesh.text = DialogsUtility.dialogs[DialogInstance.ISaidBlue];
+        }
+
+        //speciale: Dude prende in giro la tua lentezza
+        if (!blueUpperPin.IsConnected && !pinkUpperPin.IsConnected &&
+            (dialogFlowState == 7 || dialogFlowState == 3 || dialogFlowState == 11 || dialogFlowState == 9))
+        {
+            dialogFlowState = 8;
+            //nextDialogTimer = 0;
+            spriteRenderer.sprite = dudeWorried;
+            textMesh.text = DialogsUtility.dialogs[DialogInstance.LongDay];
+        }
+
+        //speciale: se fai quello che dice Dude, ti perdona
+        if (blueUpperPin.IsConnected && !pinkUpperPin.IsConnected &&
+            dialogFlowState == 8)
+        {
+            dialogFlowState = 9;
+            nextDialogTimer = 0;
+            mistakeDone = false;
+            spriteRenderer.sprite = dudeFine;
+            textMesh.text = DialogsUtility.dialogs[DialogInstance.OkBlue];
+        }
+
+        //terzo dialogo
+        if (blueUpperPin.IsConnected && !pinkUpperPin.IsConnected &&
+            (dialogFlowState == 2 || (dialogFlowState == 9 && nextDialogTimer >=5)))
+        {
+            dialogFlowState = 3;
+            //nextDialogTimer = 0;
+            spriteRenderer.sprite = dudeFine;
+            textMesh.text = DialogsUtility.dialogs[DialogInstance.Attach];
+        }
+
+        //quarto dialogo
+        if (lowerPin.IsConnected &&
+            (dialogFlowState == 3 || dialogFlowState == 7 || dialogFlowState == 11 || dialogFlowState == 9))
+        {
+            dialogFlowState = 4;
+            //nextDialogTimer = 0;
+            spriteRenderer.sprite = dudeHappy;
+            textMesh.text = DialogsUtility.dialogs[DialogInstance.Detach];
+        }
+
+        //quinto dialogo
+        if (!lowerPin.IsConnected &&
+            (dialogFlowState == 4 || dialogFlowState == 10))
+        {
+            dialogFlowState = 5;
+            //nextDialogTimer = 0;
+
+            //da sostituire con DudeTwinkling
+            spriteRenderer.sprite = dudeFine;
+            textMesh.text = DialogsUtility.dialogs[DialogInstance.Remove];
+        }
+
+        //speciale: hai attaccato il cavo anche se Dude ti ha chiesto di rimuoverlo
+        if (lowerPin.IsConnected &&
+            dialogFlowState == 5)
+        {
+            dialogFlowState = 10;
+            mistakeDone = true;
+            //nextDialogTimer = 0;
+
+            //da sostituire con DudeTwinkling
+            spriteRenderer.sprite = dudePissed;
+            textMesh.text = DialogsUtility.dialogs[DialogInstance.BadTime];
+        }
+
+        //speciale: Dude ha perso la pazienza
+        if (!lowerPin.IsConnected && !blueUpperPin.IsConnected && !pinkUpperPin.IsConnected &&
+            dialogFlowState == 5 && mistakeDone)
+        {
+            dialogFlowState = 10;
+            //nextDialogTimer = 0;
+
+            //da sostituire con DudeTwinkling
+            spriteRenderer.sprite = dudePissed;
+            textMesh.text = DialogsUtility.dialogs[DialogInstance.WasteTime];
+        }
+
+        //sesto dialogo
+        if (!blueUpperPin.IsConnected && !pinkUpperPin.IsConnected && !mistakeDone &&
+            dialogFlowState == 5)
+        {
+            dialogFlowState = 6;
+            //nextDialogTimer = 0;
+            spriteRenderer.sprite = dudeFine;
+            textMesh.text = DialogsUtility.dialogs[DialogInstance.RepeatForPink];
+        }
+
+        //speciale: finchè non fai quello che dice Dude, non si va avanti
+        if (blueUpperPin.IsConnected &&
+            dialogFlowState == 6)
+        {
+            dialogFlowState = 12;
+            //nextDialogTimer = 0;
+            spriteRenderer.sprite = dudePissed;
+            textMesh.text = DialogsUtility.dialogs[DialogInstance.IAmRuler];
+        }
+
+        //sesto dialogo
+        if (!blueUpperPin.IsConnected && pinkUpperPin.IsConnected && lowerPin.IsConnected &&
+            (dialogFlowState == 6 || dialogFlowState == 12))
+        {
+            //nextDialogTimer = 0;
+            spriteRenderer.sprite = dudeFine;
+            textMesh.text = DialogsUtility.dialogs[DialogInstance.GoalReached];
+        }
     }
 
     public void StandardBehavoiur()
--- AleXavSharedTesting/Assets/Scripts/Utility/DialogsUtility.cs
+++ AleXavSharedTesting/Assets/Scripts/Utility/DialogsUtility.cs
@@ -4,26 +4,52 @@ using UnityEngine;
 
 public static class DialogsUtility
 {
-    public static Dictionary<DialogInstance, Dialog> dialogs = new Dictionary<DialogInstance, Dialog>
+    public static Dictionary<DialogInstance, string> dialogs = new Dictionary<DialogInstance, string>
     {
-        {DialogInstance.Welcome, new Dialog("Hi Chip! How are you? I know it is your first day, I'm sure we can be good friends.", false) },
+        {DialogInstance.Welcome, "Hi Chip! How are you? I know it is your first day, I'm sure we can be good friends." },
         
-        {DialogInstance.GoalReached, new Dialog("Mission accomplished! See you on the next level.", false)},
+        {DialogInstance.GoalReached, "Mission accomplished! See you on the next level."},
         
-        {DialogInstance.Pissed, new Dialog("Are you trying to make me lose my temper?", false)},
+        {DialogInstance.Pissed, "Are you trying to make me lose my temper?"},
         
-        {DialogInstance.RedeemYourself, new Dialog("Please, drag and drop yourself to the upper pin, again...", false)},
+        {DialogInstance.RedeemYourself, "Please, drag and drop yourself to the upper pin, again..."},
         
-        {DialogInstance.Joke, new Dialog("views", false)},
+        {DialogInstance.Joke, "Are you joking?"},
         
-        {DialogInstance.GoToUpperPin, new Dialog("To begin, drag and drop yourself to the upper pin.", false)},
+        {DialogInstance.GoToUpperPin, "To begin, drag and drop yourself to the upper pin."},
 
-        {DialogInstance.GoToLowerPin, new Dialog("Great! Next, drag yourself to the lower pin.", false)},
+        {DialogInstance.GoToLowerPin, "Great! Next, drag yourself to the lower pin."},
 
-        {DialogInstance.GoToLowerPinSavagePartOne, new Dialog("Wow! Have you already attended the movement training course?", false)},
+        {DialogInstance.GoToLowerPinSavagePartOne, "Wow! Have you already attended the movement training course?"},
 
-        {DialogInstance.GoToLowerPinSavagePartTwo, new Dialog("However, now drag and drop yourself to the lower pin.", false)},
+        {DialogInstance.GoToLowerPinSavagePartTwo, "However, now drag and drop yourself to the lower pin."},
 
-        {DialogInstance.SecondTutorial, new Dialog("boh", false)}
+        {DialogInstance.CanDetach, "You must know that the cables can be also detached, of course."},
+
+        {DialogInstance.NoTimeToLose, "No time to lose, uh? I understand. Wire it to the lower pin."},
+
+        {DialogInstance.LongDay, "I think this will be a loooong day... please, restart from the blue one."},
+
+        {DialogInstance.CreateBlue, "But first, you need to begin wiring. Start from the blue pin."},
+
+        {DialogInstance.ISaidBlue, "I said blue... nevermind... wire the cable to the lower pin."},
+
+        {DialogInstance.OkBlue, "Thanks. This time I will forgive you."},
+
+        {DialogInstance.Attach, "And then, attach the cable to the lower pin."},
+
+        {DialogInstance.Detach, "Great! Now, to detach the cable, drag an drop yourself to the lower pin."},
+
+        {DialogInstance.Remove, "And next, delete it. I think you know how to do it."},
+
+        {DialogInstance.BadTime, "You're gonna have a bad time. I suggest you to detach the cable."},
+
+        {DialogInstance.RepeatForPink, "Excellent! Complete your daily job connecting the pink pin to the lower one."},
+
+        {DialogInstance.WasteTime, "Ok, I have no more time to waste. See you on the next level."},
+
+        {DialogInstance.IAmRuler, "No problem. We can stay here forever, until you wire the pink one."},
+
+        {DialogInstance.WelcomeAgain, "Hi Chip! Let's add something new today."}
     };
 }
